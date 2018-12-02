9ee413a864ddeeb3c56ce30b33b32ab9a44ee912
Ian Andrew Macalisang <ian-fedora@localhost.localdomain>
2018-12-02 23:07:15 +0400

    Initial commit

---
+++ my_temp1.js
@@ -0,0 +1,82 @@
+"use strict"
+
+import b4w from "blend4web";
+
+var m_app     = b4w.app;
+var m_data    = b4w.data;
+var m_scs     = b4w.scenes;
+var m_trans   = b4w.transform;
+var m_cfg     = b4w.config;
+var m_version = b4w.version;
+var m_preloader = b4w.preloader;
+
+var DEBUG = (m_version.type() === "DEBUG");
+
+var APP_ASSETS_PATH = m_cfg.get_assets_path("my_temp1");
+
+export function init() {
+    m_app.init({
+        canvas_container_id: "main_canvas_container",
+        callback: init_cb,
+        physics_enabled: false,
+        show_fps: true,
+        alpha: false,
+        autoresize: true,
+        assets_dds_available: !DEBUG,
+        assets_min50_available: !DEBUG,
+        console_verbose: true
+    });
+}
+
+function init_cb(canvas_elem, success) {
+
+    if (!success) {
+        console.log("b4w init failure");
+        return;
+    }
+
+    m_preloader.create_preloader();
+
+    load();
+}
+
+function load() {
+    m_data.load(APP_ASSETS_PATH + "/instancing.json", load_cb, preloader_cb);
+}
+
+/**
+ * update the app's preloader
+ */
+function preloader_cb(percentage) {
+    m_preloader.update_preloader(percentage);
+}
+
+function load_cb(data_id) {
+    m_app.enable_camera_controls(false, false, false, null, true);
+
+	// Place your code here
+    create_slider();
+	
+	var my_slider = document.getElementById("slider_id");
+	var monkey = m_scs.get_object_by_name("Suzanne");
+
+	// Update the current slider value (each time you drag the slider handle)
+	my_slider.oninput = function() {
+		m_trans.set_rotation_euler(monkey, 0, 0, this.value*3.142/50);
+	}
+ 
+}
+
+function create_slider() {
+	var slider = document.createElement("INPUT");
+    slider.id = "slider_id";
+    slider.setAttribute("type", "range");
+    slider.setAttribute("min", "-50");
+    slider.setAttribute("max", "50");
+    slider.setAttribute("value", "0");
+    slider.style.position = "relative";
+    slider.style.width = "33%";
+    document.body.appendChild(slider);
+}
+
+init();
